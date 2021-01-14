a7e53b782d4fd4b103d3db8392671a5fbd9126a7
Jerry Augusto <jerryaugusto@beinsight.com.br>
2021-01-14 16:00:33 -0400

    First version of the search engine with http requests and mathing from css selector.

---
+++ search-courses.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once  __DIR__ . '/src/SearchEngine.php';
+
+use Beyondercode\CourseFinder\SearchEngine;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$engine = new SearchEngine($client, $crawler);
+$courses = $engine->search('/cursos-online-programacao/php');
+
+foreach ($courses as $course) {
+	echo $course . PHP_EOL;
+}
---
+++ src/SearchEngine.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Beyondercode\CourseFinder;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class SearchEngine
+{
+	/**
+	 * @var ClientInterface
+	 */
+	private ClientInterface $httpClient;
+	/**
+	 * @var Crawler
+	 */
+	private Crawler $crawler;
+
+	public function __construct(ClientInterface $httpClient, Crawler $crawler)
+	{
+		$this->httpClient = $httpClient;
+		$this->crawler = $crawler;
+	}
+
+	public function search(string $url): array
+	{
+		$response = $this->httpClient->request('GET', $url);
+
+		$html = $response->getBody();
+		$this->crawler->addHtmlContent($html);
+
+		$courseElements = $this->crawler->filter('span.card-curso__nome');
+		$courses = [];
+
+		foreach ($courseElements as $element) {
+			$courses[] = $element->textContent;
+		}
+
+		return $courses;
+
+	}
+}
